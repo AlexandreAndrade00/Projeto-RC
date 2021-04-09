ffd1acce0dd1f579b9c07c3f252e7f8bed690c81
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-04-10 00:55:54 +0100

    exercicio quase feito
    precisa de organizacao e meter a interface bonita e ver se falta alguma coisa do enunciado

--- Exercicio/1/tcp_server.c
+++ Exercicio/1/tcp_server.c
@@ -13,23 +13,37 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <arpa/inet.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define SERVER_PORT 9003
 #define BUF_SIZE 1024
 
+typedef struct {
+	int nums[10];
+} sharedMemory;
+
 void process_client(int client_fd, int counter, struct sockaddr_in client_info);
 void erro(char *msg);
+
+sharedMemory *sharedVar;
+int shmid;
+key_t key;
 
 int main() {
   int fd, client;
   struct sockaddr_in addr, client_addr;
   int client_addr_size;
   int aux = 0;
+  
+  shmid = shmget(key, sizeof(sharedMemory), IPC_CREAT|0700);
+  sharedVar = (sharedMemory*) shmat(shmid, NULL, 0);
 
   bzero((void *) &addr, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
   addr.sin_port = htons(SERVER_PORT);
+   
 
   if ( (fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	erro("na funcao socket");
@@ -38,6 +52,7 @@ int main() {
   if( listen(fd, 5) < 0)
 	erro("na funcao listen");
   client_addr_size = sizeof(client_addr);
+  printf("Receving messages from %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
   while (1) {
     //clean finished child processes, avoiding zombies
     //must use WNOHANG or would block whenever a child process was working
@@ -54,24 +69,52 @@ int main() {
     close(client);
     }
   }
+  shmctl(shmid, IPC_RMID, NULL);
   return 0;
 }
 
 void process_client(int client_fd, int counter, struct sockaddr_in client_info) {
-	int nread = 0;
+	int nread = 0, aux, i;
 	char buffer[BUF_SIZE], message[100];
-  char client_ip_adress[INET_ADDRSTRLEN];
-
-  nread = read(client_fd, buffer, BUF_SIZE-1);
-  buffer[nread] = '\0';
-
-  printf("--New Message--\n");
-  inet_ntop(AF_INET, &client_info.sin_addr, client_ip_adress, INET_ADDRSTRLEN);
-  printf("Client %d, connected from %s:%d says: %s\n", client_fd, client_ip_adress,client_info.sin_port,buffer);
-  snprintf(message, 100, "Server received connection from (IP:port) %s:%d. Received %d connections", client_ip_adress, client_info.sin_port, counter);
-  write(client_fd, message, 1+strlen(message));
-
-  fflush(stdout);
+	
+	while(1) {
+		nread = read(client_fd, buffer, BUF_SIZE-1);
+		buffer[nread] = '\0';
+		printf("Client %d, connected from %s:%d says: %s\n", client_fd, inet_ntoa(client_info.sin_addr),client_info.sin_port,buffer);
+	
+		if(strcmp(buffer, "DADOS")==0) {
+			printf("Reciving numbers\n");
+			for (i=0; i<10; i++) {
+				nread = read(client_fd, buffer, BUF_SIZE-1);
+				sharedVar->nums[i] = (int) strtol(buffer, (char **) NULL, 10);
+				printf("%d ",sharedVar->nums[i]);
+				fflush(stdout);
+			}
+			printf("\nNumeres received successfully\n");
+		} else if(strcmp(buffer, "SOMA")==0){
+			aux=0;
+			for(i=0; i<10; i++) {
+				aux = aux + sharedVar->nums[i];
+			}
+			snprintf(message, 100, "Soma=%d\n", aux);
+			write(client_fd, message, 1+strlen(message));
+		} else if(strcmp(buffer, "MEDIA")==0){
+			aux=1;
+			for(i=0; i<10; i++) {
+				aux += sharedVar->nums[i];
+			}
+			float med = aux/10;
+			snprintf(message, 100, "Media=%0.2f\n", med);
+			write(client_fd, message, 1+strlen(message));
+		} else if(strcmp(buffer, "SAIR")==0) {
+			printf("Client leaved\n");
+			break;
+		}
+		else
+			erro("mensagem nao conhecida");
+			
+		printf("\n");
+	}
 	close(client_fd);
 }
 
